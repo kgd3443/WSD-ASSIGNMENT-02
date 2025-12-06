80c921bb055e99c591a1a1b87b911300bc803627
kgd3443 <shyshy7665@naver.com>
2025-12-06 21:40:08 +0900

    테스트

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import Wishlist from "./views/Wishlist";
 
 function App() {
     return (
-        <BrowserRouter>
+        <BrowserRouter basename={import.meta.env.BASE_URL}>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/signin" element={<SignIn />} />
