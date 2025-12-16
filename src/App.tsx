6d4dbec71dfdd020569a5459419743e799f4f5a4
kgd3443 <shyshy7665@naver.com>
2025-12-16 11:01:50 +0900

    오류 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 // src/App.tsx
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
@@ -12,35 +12,24 @@ import Search from "./views/Search";
 import Wishlist from "./views/Wishlist";
 import SignIn from "./views/SignIn";
 
-const basename = import.meta.env.BASE_URL; // ex) "/WSD-ASSIGNMENT-02/"
-
 function App() {
     return (
         <>
-            {/* Toast (추가 점수 요소) */}
             <ToastContainer position="top-center" autoClose={1800} />
 
-            {/* ✅ GitHub Pages / base 경로 대응 */}
-            <BrowserRouter basename={basename}>
-                <Routes>
-                    {/* ✅ 로그인 페이지는 공개 */}
-                    <Route path="/signin" element={<SignIn />} />
+            {/* ✅ Router는 main.tsx에만 두고, 여기서는 Routes만 사용 */}
+            <Routes>
+                <Route path="/signin" element={<SignIn />} />
 
-                    {/* ✅ 로그인 필요한 페이지들은 보호 */}
-                    <Route element={<RequireAuth />}>
-                        <Route path="/" element={<Layout />}>
-                            <Route index element={<Home />} />
-                            <Route path="popular" element={<Popular />} />
-                            <Route path="search" element={<Search />} />
-                            <Route path="wishlist" element={<Wishlist />} />
-                        </Route>
+                <Route element={<RequireAuth />}>
+                    <Route path="/" element={<Layout />}>
+                        <Route index element={<Home />} />
+                        <Route path="popular" element={<Popular />} />
+                        <Route path="search" element={<Search />} />
+                        <Route path="wishlist" element={<Wishlist />} />
                     </Route>
-
-                    {/* (선택) 매칭 안 되면 메인으로 보내고 싶다면 아래 추가 가능
-          <Route path="*" element={<Navigate to="/" replace />} />
-          */}
-                </Routes>
-            </BrowserRouter>
+                </Route>
+            </Routes>
         </>
     );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
+// src/main.tsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <BrowserRouter basename={import.meta.env.BASE_URL}>
+        <HashRouter>
             <App />
-        </BrowserRouter>
+        </HashRouter>
     </React.StrictMode>
 );
