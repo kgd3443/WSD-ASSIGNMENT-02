0ba9c2d7bec920852078a6cb95fa34e9b6aa03fb
kgd3443 <shyshy7665@naver.com>
2025-12-16 11:21:43 +0900

    텍스트 살짝 바꿈

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -17,7 +17,7 @@ const Header = () => {
             <div className="app-header__inner">
                 {/* 왼쪽 상단 제목 */}
                 <NavLink to="/" className="app-header__brand">
-                    🎬 MyMovie
+                    🎬
                 </NavLink>
 
                 {/* 오른쪽 메뉴 */}
